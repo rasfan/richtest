d677912b08d1fbaf37a7adc6e0f8aa1cf6f013fa
rasfan <r.sahafamin@gmail.com>
2019-01-21 15:00:22 +0330

    initial

---
+++ build/precache-manifest.12d7814d1ce1e90795fd8676b641d4ba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff13d9453ff5b0f1f708a362d37c8d",
+    "url": "/static/media/login.faff13d9.png"
+  },
+  {
+    "revision": "4d4c54030ff355b6d8c2",
+    "url": "/static/css/main.7fca6731.chunk.css"
+  },
+  {
+    "revision": "2eb8b14c130f36ad1552e71a2817ebd1",
+    "url": "/static/media/background.2eb8b14c.jpg"
+  },
+  {
+    "revision": "f8b5ff716b0b4c754274",
+    "url": "/static/js/1.f8b5ff71.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3ca6dc37109bfda0e850b92e5825967",
+    "url": "/static/media/img-user1.d3ca6dc3.png"
+  },
+  {
+    "revision": "656ad60761ab78def1bfd7fea410e4f0",
+    "url": "/static/media/logo.656ad607.png"
+  },
+  {
+    "revision": "4d4c54030ff355b6d8c2",
+    "url": "/static/js/main.4d4c5403.chunk.js"
+  },
+  {
+    "revision": "4776226217328d730ecf848a2d0b02ff",
+    "url": "/static/media/save.47762262.png"
+  },
+  {
+    "revision": "16e89a11689adc4c88d472b30ad37e4f",
+    "url": "/static/media/wel2.16e89a11.png"
+  },
+  {
+    "revision": "c9ea9efe7c7b3008491249873d8584c8",
+    "url": "/static/media/tel.c9ea9efe.png"
+  },
+  {
+    "revision": "1bc89328cd2ed90bbebbadb68cc91e28",
+    "url": "/static/media/singin.1bc89328.png"
+  },
+  {
+    "revision": "b855bc5f54bb13adec89a81a054829c2",
+    "url": "/static/media/signup.b855bc5f.png"
+  },
+  {
+    "revision": "f8b5ff716b0b4c754274",
+    "url": "/static/css/1.4073c55c.chunk.css"
+  },
+  {
+    "revision": "444847bc13f0fbeb8765983fc1e4fe49",
+    "url": "/index.html"
+  }
+];
